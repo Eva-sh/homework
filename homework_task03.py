a4cdb8e2c2c986e4903ecf60abc16dea115992ee
eva shvets <evashvets2002@gmail.com>
2023-02-23 13:30:53 +1000

    reformat code

--- homework_task03.py
+++ homework_task03.py
@@ -1,17 +1,16 @@
-from typing import Tuple,List
+from typing import Tuple, List
 
 
 def find_maximum_and_minimum(file_name: str) -> List[Tuple[int, int]]:
-    new=[]
+    new = []
     with open(file_name) as fi:
         for line in fi:
-            if line=="\n": #
+            if line == "\n":  # we exclude the case with an empty line
                 continue
-            line=line.split(",")
+            line = line.split(",")
             for i in range(len(line)):
-                line[i]=int(line[i]) #
-            new.append((min(line),max(line)))
+                line[i] = int(line[i])  # converting string elements to integers
+            new.append((min(line), max(line)))
+    return new
 
-
-
-#find_maximum_and_minimum("file_task03.txt")
+# find_maximum_and_minimum("file_task03.txt")
