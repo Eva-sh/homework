3b476b1b29a4f3ec37b0ffa405045c1e747ca91b
eva shvets <evashvets2002@gmail.com>
2023-03-30 14:53:16 +1000

    done task04

--- homework_04.py
+++ homework_04.py
@@ -1,6 +1,12 @@
 def is_armstrong(number: int) -> bool:
-    ...
+    numbers=[int(i) for i in str(number)]
+    count_numbers=len(numbers)
+    Armstrong_num=sum([i**count_numbers for i in numbers])
+    return Armstrong_num==number
 
 
-assert is_armstrong(153) == True, 'Is Armstrong number'
-assert is_armstrong(10) == False, 'Is not Armstrong number'
+
+
+
+assert is_armstrong(153) == True
+assert is_armstrong(10) == False
